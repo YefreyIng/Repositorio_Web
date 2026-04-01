f89c974f96c6aa9ae17e6011f0007f39285a2bf0
Yefrey <jeffreygomez2123@gmail.com>
2026-04-01 09:17:26 -0500

    EntregableWeb_2026

---
+++ js/main.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+function openModal(id) {
+  document.getElementById(id).style.display = 'block';
+}
+
+function closeModal(id) {
+  document.getElementById(id).style.display = 'none';
+}
